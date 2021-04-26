985f586868878bc668ad0f7689ffd46dd2108848
Rafael Brito <rafa.brito295@gmail.com>
2021-04-26 11:41:03 -0300

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-        // dd(public_path());
+        dd(realpath(dirname('faebd263ac-private.pem.key')));
         return view('index');
     }
 
